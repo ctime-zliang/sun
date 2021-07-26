5bd9f92fed0b6f46ecf724edd55046e8c9fccd46
richardadda <1156282805@qq.com>
2021-07-27 00:13:31 +0800

    feat: modify - add html-webpack-plugin plugin

--- config/webpack-lib.dev.config.js
+++ config/webpack-lib.dev.config.js
@@ -1,22 +1,31 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const utils = require('./utils')
 const webpackLibBaseConfig = require('./webpack-lib.base.config')
 const webpackLibDevServerConfig = require('./webpack-lib.dev-server.config')
 
 // const HappyThreadPoolCase = HappyPack.ThreadPool({ size: OS.cpus().length })
-const webpackDevConfig = {
+const distChildPath = `/dev`
+const webpackConfig = {
 	// target: 'web',
 	mode: 'development',
 	entry: {
 		main: utils.resolveDirectory(`./src/index.ts`)
 	},
 	output: {
-		path: utils.resolveDirectory(`./dist/dev`),
+		path: utils.resolveDirectory(`./dist/${distChildPath}`),
 		filename: `[name].js`,
 	},
 	devtool: 'source-map',
 	devServer: webpackLibDevServerConfig,
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: `./index.html`,
+			template: utils.resolveDirectory(`./src/template/index.ejs`),
+			inject: true,
+		}),
+	]
 }
 
-module.exports = merge(webpackDevConfig, webpackLibBaseConfig)
+module.exports = merge(webpackConfig, webpackLibBaseConfig)
--- config/webpack-lib.prod.config.js
+++ config/webpack-lib.prod.config.js
@@ -1,20 +1,30 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const utils = require('./utils')
 const webpackLibBaseConfig = require('./webpack-lib.base.config')
+const webpackLibDevServerConfig = require('./webpack-lib.dev-server.config')
 
 // const HappyThreadPoolCase = HappyPack.ThreadPool({ size: OS.cpus().length })
-const webpackProdConfig = {
+const distChildPath = `/prod`
+const webpackConfig = {
 	// target: 'web',
-	mode: 'production',
+	mode: 'development',
 	entry: {
 		main: utils.resolveDirectory(`./src/index.ts`)
 	},
 	output: {
-		path: utils.resolveDirectory(`./dist/prod`),
+		path: utils.resolveDirectory(`./dist/${distChildPath}`),
 		filename: `[name].js`,
 	},
 	devtool: 'source-map',
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: `./dist/${distChildPath}/index.html`,
+			template: utils.resolveDirectory(`./src/template/index.ejs`),
+			inject: true,
+		}),
+	]
 }
 
-module.exports = merge(webpackProdConfig, webpackLibBaseConfig)
+module.exports = merge(webpackConfig, webpackLibBaseConfig)
