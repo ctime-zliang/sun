482a9cda08362face9006ddd7c6d638d6b90d6cf
ctime-zliang <1156282805@qq.com>
2022-10-17 17:16:21 +0800

    feat: modify - daily commit

--- config/webpack-lib.rules.js
+++ config/webpack-lib.rules.js
@@ -13,21 +13,21 @@ const jsEsbuildLoader = {
 	},
 }
 
-// const tsEsbuildLoader = {
-// 	test: /\.(ts|tsx)$/,
-// 	exclude: /node_modules/,
-// 	loader: 'esbuild-loader',
-// 	options: {
-// 		loader: 'jsx',
-// 		target: 'es2015',
-// 		jsxFactory: 'Sun.createElement',
-// 		jsxFragment: 'Sun.Fragment',
-// 		tsconfigRaw: require('../tsconfig.json'),
-// 		minify: false,
-// 		minifyWhitespace: false,
-// 		sourcemap: true,
-// 	},
-// }
+const tsEsbuildLoader = {
+	test: /\.(ts|tsx)$/,
+	exclude: /node_modules/,
+	loader: 'esbuild-loader',
+	options: {
+		loader: 'jsx',
+		target: 'es2015',
+		jsxFactory: 'Sun.createElement',
+		jsxFragment: 'Sun.Fragment',
+		tsconfigRaw: require('../tsconfig.json'),
+		minify: false,
+		minifyWhitespace: false,
+		sourcemap: true,
+	},
+}
 
 const tsLoader = {
 	test: /\.(ts|tsx)$/,
